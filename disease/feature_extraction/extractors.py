6b8bb550e218aa1d28400e23af78eaada747a62f
DmitryPogrebnoy <pogrebnoy.inc@gmail.com>
2023-01-30 22:16:06 +0100

    Add symptom entities by rule

--- disease/feature_extraction/extractors.py
+++ disease/feature_extraction/extractors.py
@@ -1,17 +1,18 @@
-from typing import Iterable, Dict, List, Set
+from typing import Dict, Iterable, List
 
-from spacy import Language
-from spacy.tokens import Doc
-from spacy.tokens.token import Token
-
-from disease.base.estimators import BaseTransformer
 import spacy
 from negspacy.negation import Negex
+from spacy import Language
+from spacy.tokens import Doc
+
+from disease.base.estimators import BaseTransformer
+
+NEGEX_ENTITY_LABEL: str = "SYMPTOM"
 
 
 class SymptomExtractor(BaseTransformer):
-    """ Symptom based text vectorization
-    """
+    """Symptom based text vectorization"""
+
     SPACY_LANG_MODEL_NAME: str = "ru_core_news_md"
     NEGEX_EXTENSION_NAME: str = "negex"
 
@@ -65,13 +66,7 @@ class SymptomExtractor(BaseTransformer):
         "никогда",
     ]
 
-    following_negations = [
-        "отклонено",
-        "маловероятно",
-        "не было",
-        "не были",
-        "нет"
-    ]
+    following_negations = ["отклонено", "маловероятно", "не было", "не были", "нет"]
 
     termination = [
         "хотя",
@@ -93,16 +88,35 @@ class SymptomExtractor(BaseTransformer):
         "pseudo_negations": pseudo_negations,
         "preceding_negations": preceding_negations,
         "following_negations": following_negations,
-        "termination": termination
+        "termination": termination,
     }
 
     # TODO: Построить нормальную модель рассматриваемых симптомов
-    symptoms = ["температура", "недомогание", "болит голова"]
+    symptoms = ["температура", "недомогание", "болеть голова"]
+    patterns = [
+        {"label": NEGEX_ENTITY_LABEL, "pattern": [{"LEMMA": "температура"}]},
+        {"label": NEGEX_ENTITY_LABEL, "pattern": [{"LEMMA": "недомогание"}]},
+        {
+            "label": NEGEX_ENTITY_LABEL,
+            "pattern": [{"LEMMA": "болеть"}, {"LEMMA": "голова"}],
+        },
+    ]
 
     def __init__(self):
-        self._spacy_lang_model: Language = spacy.load(SymptomExtractor.SPACY_LANG_MODEL_NAME)
-        self._negex_model: Negex = Negex(self._spacy_lang_model, "test", SymptomExtractor.russian_termset,
-                                         list(), SymptomExtractor.NEGEX_EXTENSION_NAME, list())
+        self._spacy_lang_model: Language = spacy.load(
+            SymptomExtractor.SPACY_LANG_MODEL_NAME
+        )
+        ruler = self._spacy_lang_model.add_pipe("entity_ruler")
+
+        ruler.add_patterns(SymptomExtractor.patterns)
+        self._negex_model: Negex = Negex(
+            nlp=self._spacy_lang_model,
+            name="negotiation",
+            neg_termset=SymptomExtractor.russian_termset,
+            ent_types=[NEGEX_ENTITY_LABEL],
+            extension_name=SymptomExtractor.NEGEX_EXTENSION_NAME,
+            chunk_prefix=[],
+        )
 
     def fit(self, x: Iterable[str]):
         pass
@@ -118,25 +132,15 @@ class SymptomExtractor(BaseTransformer):
         message_disease: Dict[str, int] = dict.fromkeys(SymptomExtractor.symptoms, 0)
 
         # Проверяем есть ли в entities симптомы и проставляем соответствующие метки
-        entities: Dict[str, bool] = dict(negex_doc.ents)
+        entities: Dict[str, bool] = {
+            entity.lemma_: entity._.negex for entity in negex_doc.ents
+        }
         if entities:
             for symptom in SymptomExtractor.symptoms:
                 if symptom in entities:
                     if entities[symptom]:
-                        message_disease[symptom] = 1
-                    else:
                         message_disease[symptom] = -1
-
-        # TODO: Support multiwords symptoms
-        for token in model_doc:
-            if token.lemma_ in SymptomExtractor.symptoms:
-                ancestors: Set[Token] = set(token.ancestors)
-                children: Set[Token] = set(token.children)
-                if any(ancestor.lemma_ in SymptomExtractor.preceding_negations for ancestor in ancestors):
-                    message_disease[token.lemma_] = -1
-                elif any(child.lemma_ in SymptomExtractor.following_negations for child in children):
-                    message_disease[token.lemma_] = -1
-                else:
-                    message_disease[token.lemma_] = 1
+                    else:
+                        message_disease[symptom] = 1
 
         return list(message_disease.values())
--- disease/tests/extractors/test_symptom_extractor.py
+++ disease/tests/extractors/test_symptom_extractor.py
@@ -1,16 +1,19 @@
-import unittest
-
-import spacy
-
 from disease.feature_extraction import SymptomExtractor
-from spacy.lang.ru import Russian
 
 
-class SymptomExtractorTest(unittest.TestCase):
+def test_transform_on_simple_message_1():
+    message = "У меня нет температуры, но есть недомогание и не болит голова."
+    disease_vector = SymptomExtractor().transform(message)
+    assert disease_vector == [-1, 1, -1]
 
-    def test_transform_1(self):
-        message = "У меня нет температуры, но есть недомогание и не болит голова."
-        disease_vector = SymptomExtractor().transform(message)
-        self.assertEqual(disease_vector,
-                         [-1, -1, 0],
-                         "Extracted disease vector is incorrect")
+
+def test_transform_on_simple_message_2():
+    message = "У меня нет температуры и нет недомогания."
+    disease_vector = SymptomExtractor().transform(message)
+    assert disease_vector == [-1, -1, 0]
+
+
+def test_transform_on_cross_symptom():
+    message = "У меня нет температуры и есть температура, я запутался."
+    disease_vector = SymptomExtractor().transform(message)
+    assert disease_vector == [-1, 0, 0]
