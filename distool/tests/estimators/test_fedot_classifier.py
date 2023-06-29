c763aba0e504028162b79e75016d7eca0dc46783
Мичил Егоров <egorov_michil@mail.ru>
2023-06-30 01:25:36 +0300

    [feat] add time delay

--- distool/tests/estimators/test_fedot_classifier.py
+++ distool/tests/estimators/test_fedot_classifier.py
@@ -1,3 +1,5 @@
+import time
+
 from sklearn.metrics import accuracy_score
 
 from distool.estimators import FedotDiseaseClassifier
@@ -13,12 +15,13 @@ def test_classifier(complex_data):
     classifier = FedotDiseaseClassifier(early_stopping_iterations=2)
 
     # bug with nans
-    for _ in range(10):
+    for _ in range(100):
         try:
             classifier.fit(features, diseases)
             break
         except ValueError as e:
             print(repr(e), "Trying...")
+            time.sleep(1)
 
     accuracy = accuracy_score(classifier.predict(features), diseases)
 
